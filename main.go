6b966205a1df11e207e224b19c063d1e84966f56
Bruno Miguel Custódio <brunomcustodio@gmail.com>
2020-09-04 13:56:55 +0100

    Improve how quorum is checked by attempting to write.
    Signed-off-by: Bruno Miguel Custódio <brunomcustodio@gmail.com>

--- main.go
+++ main.go
@@ -15,6 +15,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -23,6 +24,7 @@ import (
 	etcdclusterv1beta2 "github.com/coreos/etcd-operator/pkg/apis/etcd/v1beta2"
 	etcdclustersclient "github.com/coreos/etcd-operator/pkg/generated/clientset/versioned"
 	log "github.com/sirupsen/logrus"
+	etcdv3client "go.etcd.io/etcd/clientv3"
 	corev1 "k8s.io/api/core/v1"
 	kubeerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -32,6 +34,14 @@ import (
 	podutils "k8s.io/kubernetes/pkg/api/v1/pod"
 
 	"github.com/form3tech-oss/cilium-etcd-watchdog/internal/version"
+)
+
+type EtcdClusterQuorumStatus int
+
+const (
+	EtcdClusterQuorumStatusLost EtcdClusterQuorumStatus = iota
+	EtcdClusterQuorumStatusOK
+	EtcdClusterQuorumStatusUnknown
 )
 
 func birthCry(kubeClient kubernetes.Interface) {
@@ -66,14 +76,60 @@ func getCiliumEtcdEtcdClusterResource(etcdClient etcdclustersclient.Interface, c
 	return c, nil
 }
 
+func getEtcdClusterQuorumStatus(kubeClient kubernetes.Interface, etcdCluster *etcdclusterv1beta2.EtcdCluster, dialTimeout, opTimeout time.Duration) (EtcdClusterQuorumStatus, error) {
+	// Compute the quorum size for Cilium's etcd cluster.
+	q := (etcdCluster.Spec.Size / 2) + (etcdCluster.Spec.Size % 2)
+
+	// List pods belonging to Cilium's etcd cluster.
+	p, err := listEtcdClusterPods(kubeClient, etcdCluster)
+	if err != nil {
+		return EtcdClusterQuorumStatusUnknown, fmt.Errorf("failed to list pods for etcdcluster: %v", err)
+	}
+
+	// Check how many pods are actually running and ready.
+	e := make([]string, 0)
+	r := 0
+	for _, pp := range p {
+		if isPodRunningAndReady(pp) {
+			r++
+		}
+		// Compute and store the etcd endpoint corresponding to this pod for eventual later usage.
+		e = append(e, fmt.Sprintf("http://%s:2379", pp.Status.PodIP))
+	}
+	// If there are less pods running and ready than the quorum size, report quorum as having been lost.
+	if r < q {
+		return EtcdClusterQuorumStatusLost, nil
+	}
+
+	// Create an etcd client and see if we can actually write something to the cluster.
+	etcdConfig := etcdv3client.Config{
+		DialTimeout: dialTimeout,
+		Endpoints:   e,
+	}
+	etcdClient, err := etcdv3client.New(etcdConfig)
+	if err != nil {
+		return EtcdClusterQuorumStatusUnknown, fmt.Errorf("failed to create etcd client: %v", err)
+	}
+	defer etcdClient.Close()
+	ctx, fn := context.WithTimeout(context.Background(), opTimeout)
+	defer fn()
+	if _, err = etcdClient.Put(ctx, "cilium-etcd-watchdog", time.Now().String()); err != nil {
+		return EtcdClusterQuorumStatusUnknown, fmt.Errorf("failed to wrtie to Cilium's etcd cluster: %v", err)
+	}
+	return EtcdClusterQuorumStatusOK, nil
+}
+
 func main() {
 	// Parse command-line flags.
 	clusterBootstrapGracePeriod := flag.Duration("cluster-bootstrap-grace-period", 2*time.Minute, "The period, measured since the creation of the EtcdCluster resource, during which we wait for the cluster to be marked as available")
 	clusterName := flag.String("cluster-name", "cilium-etcd", "The name of the etcd cluster used by Cilium")
 	clusterNamespace := flag.String("cluster-namespace", "kube-system", "The namespace where the etcd cluster used by Cilium is deployed")
+	etcdClientDialTimeout := flag.Duration("etcd-client-dial-timeout", 2*time.Second, "The timeout to use when attempting to connect to Cilium's etcd cluster")
+	etcdClientOpTimeout := flag.Duration("etcd-client-op-timeout", 5*time.Second, "The timeout to use when attempting to read or write from Cilium's etcd cluster")
 	logLevel := flag.String("log-level", log.InfoLevel.String(), "The log level to use")
+	maxQuorumStatusCheckFailures := flag.Int("max-quorum-status-check-failures", 3, "The maximum number of failed attempts to check if Cilium's etcd cluster has quorum before forcibly declaring quorum to be lost")
 	pathToKubeconfig := flag.String("path-to-kubeconfig", "", "The path to the kubeconfig file to use")
-	pollingInterval := flag.Duration("polling-interval", 5*time.Second, "The interval at which to poll the health of the etcd cluster used by Cilium")
+	pollingInterval := flag.Duration("polling-interval", 10*time.Second, "The interval at which to poll the health of the etcd cluster used by Cilium")
 	flag.Parse()
 
 	// Configure logging.
@@ -93,6 +149,7 @@ func main() {
 	// Birth cry.
 	birthCry(kubeClient)
 
+	f := 0
 	t := time.NewTicker(*pollingInterval)
 	defer t.Stop()
 	for {
@@ -119,13 +176,22 @@ func main() {
 			}
 
 			// After the grace period has elapsed, it is assumed that the cluster must have quorum.
-			// If it doesn't, we delete the target 'EtcdCluster' and wait for it to be recreated by whatever created it in the first place.
-			q, err := etcdClusterHasQuorum(kubeClient, c)
-			if err != nil {
-				log.Errorf("Failed to check if Cilium's etcd cluster has quorum: %v", err)
+			// If it doesn't, we eventually delete the target 'EtcdCluster' and wait for it to be recreated by whatever created it in the first place.
+			q, err := getEtcdClusterQuorumStatus(kubeClient, c, *etcdClientDialTimeout, *etcdClientOpTimeout)
+			switch q {
+			case EtcdClusterQuorumStatusOK:
+				f = 0
+				log.Debug("Cilium's etcd cluster has quorum")
 				continue
-			}
-			if !q {
+			case EtcdClusterQuorumStatusUnknown:
+				f++
+				log.Warnf("Failed to check if Cilium's etcd cluster has quorum: %v", err)
+				if f < *maxQuorumStatusCheckFailures {
+					continue
+				}
+				fallthrough
+			case EtcdClusterQuorumStatusLost:
+				f = 0
 				log.Warn("Cilium's etcd has lost quorum, deleting the 'EtcdCluster' resource")
 				if err := etcdClient.EtcdV1beta2().EtcdClusters(c.Namespace).Delete(c.Name, metav1.NewDeleteOptions(0)); err != nil {
 					log.Warnf("Failed to delete the 'EtcdCluster' resource: %v", err)
@@ -133,26 +199,6 @@ func main() {
 			}
 		}
 	}
-}
-
-func etcdClusterHasQuorum(kubeClient kubernetes.Interface, etcdCluster *etcdclusterv1beta2.EtcdCluster) (bool, error) {
-	// Compute the quorum size for Cilium's etcd cluster.
-	q := (etcdCluster.Spec.Size / 2) + (etcdCluster.Spec.Size % 2)
-
-	// List pods belonging to Cilium's etcd cluster.
-	p, err := listEtcdClusterPods(kubeClient, etcdCluster)
-	if err != nil {
-		return false, fmt.Errorf("failed to list pods for etcdcluster: %v", err)
-	}
-
-	// Check how many pods are actually running and ready and return whether the cluster has quorum.
-	r := 0
-	for _, pod := range p {
-		if isPodRunningAndReady(pod) {
-			r++
-		}
-	}
-	return r >= q, nil
 }
 
 func etcdClusterHasBootstrapped(etcdCluster *etcdclusterv1beta2.EtcdCluster) bool {
